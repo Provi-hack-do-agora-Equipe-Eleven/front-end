4ad47ecc14f98cbf247064e9ee1bb769547df253
Gabriel Trettin <gtcastro7@gmail.com>
2022-04-29 00:25:01 -0300

    feat: i18n setup

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,5 +1,95 @@
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { MenuItem, Select } from "@mui/material";
+import "../styles/App.css";
+
 const App = () => {
-  return <></>;
+  const {
+    t,
+    i18n: { changeLanguage, language },
+  } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState("us");
+  const [open, setOpen] = useState(false);
+
+  const countryLangDict: any = {
+    us: "en",
+    br: "pt",
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleChange = (e: any) => {
+    console.log("handleChangeEvent", e);
+    setSelectedLanguage(e);
+    changeLanguage(countryLangDict[e.toLowerCase()]);
+
+    console.log(
+      "handleChange changeLang value",
+      countryLangDict[e.toLowerCase()]
+    );
+  };
+
+  function getKeyByValue(object: any, value: string) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+
+  useEffect(() => {
+    if (language) {
+      const userLangExists = getKeyByValue(
+        countryLangDict,
+        language.slice(0, 2)
+      );
+      if (userLangExists) {
+        setSelectedLanguage(userLangExists);
+      }
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <div className="select-language">
+        <label
+          id="demo-controlled-open-select-label"
+          className="lang-label"
+          onClick={handleOpen}
+          style={{
+            padding: "4px",
+          }}
+        >
+          <img src={`countries-flags/${selectedLanguage}.svg`} alt="" />
+        </label>
+        <Select
+          className="hide"
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          value={selectedLanguage}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          onChange={(e) => handleChange(e.target.value)}
+        >
+          {Object.keys(countryLangDict).map((country) => {
+            return (
+              <MenuItem key={country} value={country}>
+                <img
+                  src={`countries-flags/${country}.svg`}
+                  alt={country + " flag"}
+                />
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </div>
+    </div>
+  );
 };
 
 export default App;
