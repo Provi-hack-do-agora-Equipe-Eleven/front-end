f8cf74e2d94b06188247fe5b8a98320de660ff60
Gabriel Trettin <gtcastro7@gmail.com>
2022-04-29 13:15:20 -0300

    refactor: changed App style to App folder

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { MenuItem, Select } from "@mui/material";
-import "../styles/App.css";
+import "./App.css";
 
 const App = () => {
   const {
