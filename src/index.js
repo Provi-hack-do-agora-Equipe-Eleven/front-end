b7d32d7b122068c95526f42111100719fa11a8b4
Gabriel Trettin <gtcastro7@gmail.com>
2022-04-29 20:50:45 -0300

    BREAKING_CHANGES: refactored project to use JS instead of Typescript. Refactored internacionalization to LanguageSelector component.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./views/App/App";
 import "./i18n";
+import App from "./views/App/App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
