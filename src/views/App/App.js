13fe679f0987bddd20839bf82558c6a9ed48c877
Gabriel Trettin <gtcastro7@gmail.com>
2022-04-30 15:31:09 -0300

    feat: created header component and implemented Language Selector

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,7 +1,13 @@
+import Header from "../../components/Header";
+import LanguageSelector from "../../components/LanguageSelector/LanguageSelector";
 import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
