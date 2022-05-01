977e9e8169cb450c94806d31ab6ba16aca725802
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-05-01 02:28:07 -0300

    fix: changed ProjectCard styles and add ProjectCard background

--- src/components/ProjectsMain/styles.js
+++ src/components/ProjectsMain/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import background from "../../assets/images/projects-main-background.png";
+import background from "../../assets/images/projects-main-background.png"
 
 export const Container = styled.article`
   width: 100%;
