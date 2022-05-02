2d209df45c2031ab7f5a5c61a2c7dc88ab2f2ec1
Gabriel Trettin <gtcastro7@gmail.com>
2022-05-01 22:51:52 -0300

    feat: added translations

--- src/components/ProjectHeader/index.js
+++ src/components/ProjectHeader/index.js
@@ -1,26 +1,35 @@
-import plasticCaps from '../../assets/icons/plasticCaps.svg'
-import { Link } from 'react-router-dom'
+import plasticCaps from "../../assets/icons/plasticCaps.svg";
+import { Link } from "react-router-dom";
 import "./style.css";
+import { useTranslation } from "react-i18next";
 
 const ProjectHeader = () => {
-    return (
-        <div>
-            <div className='project-header'>
-                <img src={plasticCaps} />
-                <div className='project-title'>
-                    <h1>Tampinha</h1>
-                    <h1>legal</h1>
-                </div>
-            </div>
-            <div className='project-routes'>
-                <Link to='/'><span>Home</span></Link>
-                <span> / </span>
-                <Link to='/projects'><span>Projetos</span></Link>
-                <span> / </span>
-                <Link to='#'><span>Tampinha legal</span></Link>
-            </div>
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <div className="project-header">
+        <img src={plasticCaps} />
+        <div className="project-title">
+          <h1>Tampinha</h1>
+          <h1>legal</h1>
         </div>
-    );
+      </div>
+      <div className="project-routes">
+        <Link to="/">
+          <span>Home</span>
+        </Link>
+        <span> / </span>
+        <Link to="/projects">
+          <span>{t("projects")}</span>
+        </Link>
+        <span> / </span>
+        <Link to="#">
+          <span>Tampinha legal</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectHeader;
--- src/components/ProjectInfo/index.js
+++ src/components/ProjectInfo/index.js
@@ -1,31 +1,45 @@
-import SocialMedia from './SocialMedia';
+import SocialMedia from "./SocialMedia";
 import "./style.css";
+import { useTranslation } from "react-i18next";
 
 const ProjectInfo = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <div className='flex'>
-        <div className='project-info'>
+      <div className="flex">
+        <div className="project-info">
           <h3>COLETA DE PLÁSTICO</h3>
-          <p>O programa incentiva as pessoas a recolher todo tipo de tampa plástica e depois doa o dinheiro obtido com a venda do material a diversas entidades assistenciais (como ONGs e Apaes).
-            Criado em Porto Alegre (RS), hoje tem mais de 2.000 pontos de coleta espalhados pelo Brasil e arrecadou mais de R$ 700 mil entre 2016 e 2020, recolhendo mais de 222 milhões de tampinhas.
-            O programa não recebe comissões e/ou gratificações sobre o material coletado. Todo o valor arrecadado com a venda das tampinhas é destinados 100% para entidades assistenciais participantes.</p>
+          <p>
+            O programa incentiva as pessoas a recolher todo tipo de tampa
+            plástica e depois doa o dinheiro obtido com a venda do material a
+            diversas entidades assistenciais (como ONGs e Apaes). Criado em
+            Porto Alegre (RS), hoje tem mais de 2.000 pontos de coleta
+            espalhados pelo Brasil e arrecadou mais de R$ 700 mil entre 2016 e
+            2020, recolhendo mais de 222 milhões de tampinhas. O programa não
+            recebe comissões e/ou gratificações sobre o material coletado. Todo
+            o valor arrecadado com a venda das tampinhas é destinados 100% para
+            entidades assistenciais participantes.
+          </p>
         </div>
-        <div className='project-company'>
-          <div className='project-social'>
-            <div className='border-social flex'>
-              <p>Redes Sociais</p>
+        <div className="project-company">
+          <div className="project-social">
+            <div className="border-social flex">
+              <p>{t("socialMedia")}</p>
               <SocialMedia />
             </div>
-            <p className='company-info'>O Tampinha Legal é o maior programa socioambiental de caráter educativo em 
-              economia circular de iniciativa da indústria de transformação do plástico da América Latina.</p>
+            <p className="company-info">
+              O Tampinha Legal é o maior programa socioambiental de caráter
+              educativo em economia circular de iniciativa da indústria de
+              transformação do plástico da América Latina.
+            </p>
           </div>
         </div>
       </div>
-      <div className='information flex'>
-        <button>Participar</button>
-        <button>Apoiar</button>
-        <p>Compartilhe</p>
+      <div className="information flex">
+        <button>{t("participate")}</button>
+        <button>{t("support")}</button>
+        <p>{t("share")}</p>
         <SocialMedia />
       </div>
     </div>
