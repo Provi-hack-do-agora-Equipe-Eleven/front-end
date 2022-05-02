bcda4aa35e14b69c39f9dd3bb18273cd9e8e25c4
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-05-01 22:02:42 -0300

    feat: create CreateProjectPage

---
+++ src/pages/CreateProjectPage/index.js
@@ -0,0 +1,13 @@
+import CreateProjectMain from "../../components/CreateProjectMain";
+import CreateProjectForm from "../../components/CreateProjectForm";
+
+const HomePage = () => {
+  return (
+    <>
+      <CreateProjectMain />
+      <CreateProjectForm />
+    </>
+  );
+};
+
+export default HomePage;
