d78ed20cba5fa5308d34fa9b9fd3ec1628ca47bc
Gabriel Trettin <gtcastro7@gmail.com>
2022-05-01 21:46:26 -0300

    fix: ProjectsContainer utils import

--- src/components/ProjectsContainer/index.js
+++ src/components/ProjectsContainer/index.js
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Container, Form, Select, Input, Button, Toggler, Projects } from "./styles";
+import {
+  Container,
+  Form,
+  Select,
+  Input,
+  Button,
+  Toggler,
+  Projects,
+} from "./styles";
 import ProjectCard from "../ProjectCard";
-import api from '../../services/api';
-import { states } from '../../utils/utils';
+import api from "../../services/api";
+import { states } from "../../utils/utils.js";
 
 const ProjectsContainer = () => {
   const { t } = useTranslation();
@@ -13,10 +21,10 @@ const ProjectsContainer = () => {
 
   useEffect(() => {
     api.get("/projects").then((res) => {
-      setProjects(res.data.projects)
+      setProjects(res.data.projects);
     });
   }, []);
-  
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
     if (!city) return;
@@ -37,16 +45,29 @@ const ProjectsContainer = () => {
         />
         <Select>
           <option value="">{"Estado"}</option>
-          {states.map((state) =>(
-            <option key={state} value={state}>{state}</option>
+          {states.map((state) => (
+            <option key={state} value={state}>
+              {state}
+            </option>
           ))}
         </Select>
         <Button>{t("filter")}</Button>
       </Form>
       <Projects>
-        {!moreProjects ? (
-          projects.map((project, i) => (
-            i < 6 && (
+        {!moreProjects
+          ? projects.map(
+              (project, i) =>
+                i < 6 && (
+                  <ProjectCard
+                    key={project.id}
+                    id={project.id}
+                    title={project.name}
+                    source={project.project_image}
+                    description={project.description}
+                  />
+                )
+            )
+          : projects.map((project) => (
               <ProjectCard
                 key={project.id}
                 id={project.id}
@@ -54,21 +75,9 @@ const ProjectsContainer = () => {
                 source={project.project_image}
                 description={project.description}
               />
-            )
-          ))
-        ) : (
-          projects.map((project) => (
-            <ProjectCard
-              key={project.id}
-              id={project.id}
-              title={project.name}
-              source={project.project_image}
-              description={project.description}
-            />
-          )))
-        }
+            ))}
       </Projects>
-      {projects.length > 5 &&(
+      {projects.length > 5 && (
         <Toggler>
           <Button onClick={handleToggle}>
             {!moreProjects ? t("moreProjects") : t("lessProjects")}
