045474e896f84e8a63875d10d6744d80c3dbd640
Gabriel Trettin <gtcastro7@gmail.com>
2022-05-01 23:21:51 -0300

    feat: implemented navigate on project

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,20 +1,21 @@
-import { Container, CardImage, CardContent, Button } from './styles';
-// import { useNavigate } from 'react-router-dom';
+import { Container, CardImage, CardContent, Button } from "./styles";
+import { useNavigate } from "react-router-dom";
 // import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const ProjectCard = ({ id, title, description, source }) => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
   return (
     <Container>
-      <CardImage src={source} alt='title'/>
+      <CardImage src={source} alt="title" />
       <CardContent>
         <h1>{title}</h1>
         <p>{description}</p>
       </CardContent>
-      <Button onClick={()=> console.log('click')}>
-        {t('seeMore')}
-      </Button>
+      <Button onClick={() => navigate("/project")}>{t("seeMore")}</Button>
     </Container>
   );
 };
